48c465f9bf746dcc057c5d4445da959c61a05cf2
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-22 01:31:53 -0800

    complete challenege 6

--- instagramDataClone/igDatabaseClone-EXERCISES.sql
+++ instagramDataClone/igDatabaseClone-EXERCISES.sql
@@ -43,4 +43,13 @@ SELECT
 (SELECT Count(*) FROM users) 
 AS avg; 
 
--- Challenge 6: 
+-- Challenge 6: What are the top 5 most commonly used hashtags?
+SELECT
+    tags.tag_name, 
+    Count(*) AS total
+FROM photo_tags 
+INNER JOIN tags 
+    ON photo_tags.tag_id = tags.id 
+GROUP BY tags.id 
+ORDER BY total DESC 
+LIMIT 5; 
