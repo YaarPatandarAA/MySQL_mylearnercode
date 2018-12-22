b8486598475f7627d7f46453f5d52453a8c6b031
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-22 00:44:06 -0800

    Create createAllTables.sql
    Add a create All tables file

---
+++ instagramDataClone/createAllTables.sql
@@ -0,0 +1,14 @@
+-- 1
+source createTables/createIGUsersTable.sql
+-- 2
+source createTables/createIGPhotosTable.sql
+-- 3
+source createTables/createIGCommentsTable.sql
+-- 4
+source createTables/createIGLikesTable.sql
+-- 5
+source createTables/createIGFollowersTable.sql
+-- 6
+source createTables/createIGTagsTables.sql
+
+SHOW TABLES;
