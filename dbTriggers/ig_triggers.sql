d6460f401c840f10072c0ffb2b6c4323ab82ba87
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-26 23:43:56 -0800

    database triggers

---
+++ dbTriggers/ig_triggers.sql
@@ -0,0 +1,22 @@
+DELIMITER $$
+
+CREATE TRIGGER example_cannot_follow_self
+     BEFORE INSERT ON follows FOR EACH ROW
+     BEGIN
+          IF NEW.follower_id = NEW.followee_id
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Cannot follow yourself, silly';
+          END IF;
+     END;
+$$
+
+CREATE TRIGGER create_unfollow
+    AFTER DELETE ON follows FOR EACH ROW 
+BEGIN
+    INSERT INTO unfollows
+    SET follower_id = OLD.follower_id,
+        followee_id = OLD.followee_id;
+END$$
+
+DELIMITER ;
